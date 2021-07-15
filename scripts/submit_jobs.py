d089be6ae5fc4ca96514b20b00ae1469f0c3c0b3
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2021-07-15 14:18:43 -0300

    tune jobs

--- scripts/submit_jobs.py
+++ scripts/submit_jobs.py
@@ -13,7 +13,7 @@ def print_and_run(cmd: str):
 
 config_files = [path.abspath("configs/" + f) for f in listdir(
     "configs/") if isfile(join("configs/", f))]
-command = "/usr/bin/sbatch --job-name={job_name}.job -c 1 /home/gabriel-milan/GIT_REPOS/wnn/scripts/run.sh {config_file}"
+command = "/usr/bin/sbatch --job-name={job_name}.job -n 1 --mem-per-cpu=8G -c 1 /home/gabriel-milan/GIT_REPOS/wnn/scripts/run.sh {config_file}"
 
 i = 0
 for config_file in sample(config_files, k=SUBSET_SIZE):
