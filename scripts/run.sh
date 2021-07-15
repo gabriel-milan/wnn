4bc3bd12e50e54b1b04893d154642ad6fb8c91e1
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2021-07-15 14:15:20 -0300

    change repo name

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,4 +2,4 @@
 
 export DB_CONNECTION_URL=""
 source /usr/local/Miniconda3-py37_4.8.3-Linux-x86_64/etc/profile.d/conda.sh && conda activate rnsp
-cd ~/GIT_REPOS/RNSP/t1/prod && python3 main.py $1
+cd ~/GIT_REPOS/wnn && python3 main.py $1
